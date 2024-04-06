51eb9b94c17bc0e1e6ce4da027cb2843b353659f
dev-chpark <xpmxf5@gmail.com>
2024-04-06 17:35:39 +0900

    ⭐ feat: lv2. TargetNum

---
+++ src/main/java/org/example/lv1/dfsBfs/TargetNum.java
@@ -0,0 +1,32 @@
+package org.example.lv1.dfsBfs;
+
+public class TargetNum {
+
+    private static int answer = 0;
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(new int[]{1, 1, 1, 1, 1}, 3));   // 5
+        System.out.println(solution(new int[]{4, 1, 2, 1}, 4));     // 2
+    }
+
+    private static int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+
+    private static void dfs(int start_idx, int temp, int[] numbers, int target) {
+
+        if (start_idx == numbers.length) {
+            if (temp == target) {
+                answer++;
+            }
+            return;
+        }
+
+        int currVal = numbers[start_idx];
+
+        dfs(start_idx + 1, temp + currVal, numbers, target);
+        dfs(start_idx + 1, temp - currVal, numbers, target);
+    }
+}
