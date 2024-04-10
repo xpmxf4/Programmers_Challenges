aa95bd06d71e5d1a2f292f5d8130960c41bb6cc3
dev-chpark <xpmxf5@gmail.com>
2024-04-10 17:51:54 +0900

    ⭐ feat: lv3. PhoneBook

---
+++ src/main/java/org/example/lv3/hash/PhoneBook.java
@@ -0,0 +1,30 @@
+package org.example.lv3.hash;
+
+import java.util.Arrays;
+
+public class PhoneBook {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"12", "123", "1235", "567", "88"}));
+    }
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        // sorting
+        Arrays.sort(phone_book);
+
+        // loop & check
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String curr_phone = phone_book[i];
+            String next_phone = phone_book[i + 1];
+
+            if (next_phone.startsWith(curr_phone)) {
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
